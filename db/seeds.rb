5a7d1b56e3b425be31d38cbf960adc39295de85a
kaushik shamantha <kaushik.s@fidenz.com>
2020-06-25 15:49:27 +0530

    seed update

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# User.create!(email: 'admin@example.com', password: '123123', password_confirmation: '123123') if Rails.env.development?
+User.create!(email: 'admin@example.com', password: '123123', password_confirmation: '123123')
 Rich::StorageFolder.create!(folder_name: 'parent', parent_id: '-1')
